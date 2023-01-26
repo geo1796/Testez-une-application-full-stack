74b24ca8a775a07a2a086176014225717557a3e0
Geoffrey Ouvrard <geoffreyouvrard@macbook-pro-de-geoffrey.home>
2023-01-26 16:50:43 +0100

    added unit tests for TeacherController

--- back/src/main/java/com/openclassrooms/starterjwt/dto/TeacherDto.java
+++ back/src/main/java/com/openclassrooms/starterjwt/dto/TeacherDto.java
@@ -1,6 +1,7 @@
 package com.openclassrooms.starterjwt.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class TeacherDto {
     private Long id;
     @NotBlank
---
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/TeacherControllerTest.java
@@ -0,0 +1,88 @@
+package com.openclassrooms.starterjwt.controllers;
+
+import com.openclassrooms.starterjwt.dto.TeacherDto;
+import com.openclassrooms.starterjwt.mapper.TeacherMapper;
+import com.openclassrooms.starterjwt.models.Teacher;
+import com.openclassrooms.starterjwt.services.TeacherService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@AutoConfigureMockMvc
+class TeacherControllerTest {
+    @Mock
+    private TeacherMapper teacherMapper;
+    @Mock
+    private TeacherService teacherService;
+
+    @Test
+    public void testFindByIdOk() {
+        Long id = 1L;
+        String firstname = "hello";
+        String lastname = "world";
+        Teacher teacher = Teacher.builder().id(id).firstName(firstname).lastName(lastname).build();
+        TeacherDto teacherDto = TeacherDto.builder().id(id).firstName(firstname).lastName(lastname).build();
+
+        when(teacherService.findById(id)).thenReturn(teacher);
+        when(teacherMapper.toDto(teacher)).thenReturn(teacherDto);
+
+        TeacherController teacherController = new TeacherController(teacherService, teacherMapper);
+        ResponseEntity<?> response = teacherController.findById(""+id);
+        TeacherDto responseBody = (TeacherDto) response.getBody();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(teacherDto, responseBody);
+    }
+
+    @Test
+    public void testFindByIdNotFound() {
+        Long id = 1L;
+
+        when(teacherService.findById(id)).thenReturn(null);
+
+        TeacherController teacherController = new TeacherController(teacherService, teacherMapper);
+        ResponseEntity<?> response = teacherController.findById(""+id);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    @Test
+    public void testFindByIdBadRequest() {
+        TeacherController teacherController = new TeacherController(teacherService, teacherMapper);
+        ResponseEntity<?> response = teacherController.findById("abc");
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void testFindAll() {
+        List<Teacher> entities = new ArrayList<>();
+        entities.add(Teacher.builder().id(1L).build());
+        entities.add(Teacher.builder().id(2L).build());
+
+        List<TeacherDto> dtos = new ArrayList<>();
+        dtos.add(TeacherDto.builder().id(1L).build());
+        dtos.add(TeacherDto.builder().id(2L).build());
+
+        when(teacherService.findAll()).thenReturn(entities);
+        when(teacherMapper.toDto(entities)).thenReturn(dtos);
+
+        TeacherController teacherController = new TeacherController(teacherService, teacherMapper);
+        ResponseEntity<?> response = teacherController.findAll();
+        List<TeacherDto> responseBody = (List<TeacherDto>) response.getBody();
+        assertEquals(dtos, responseBody);
+    }
+}
