6f9f9cb826355e18c0a0f7ef68e434829b98487b
Geoffrey Ouvrard <geoffreyouvrard@macbook-pro-de-geoffrey.home>
2023-01-30 19:16:49 +0100

    added unit tests for session-api.service

--- front/src/app/features/sessions/components/form/form.component.spec.ts
+++ front/src/app/features/sessions/components/form/form.component.spec.ts
@@ -56,4 +56,5 @@ describe('FormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
--- front/src/app/features/sessions/services/session-api.service.spec.ts
+++ front/src/app/features/sessions/services/session-api.service.spec.ts
@@ -8,6 +8,7 @@ import { SessionApiService } from './session-api.service';
 describe('SessionsService', () => {
   let service: SessionApiService;
   let controller: HttpTestingController;
+  let apiUrl: string = 'api/session';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -23,15 +24,76 @@ describe('SessionsService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('get all sessions', () => {
+  it('should get all sessions', () => {
     const expectedSessions: Session[] = [{ name: 'hello', description: 'world', date: new Date(), teacher_id: 1, users: [1] }];
     var actualSessions: Session[] = [];
     service.all().subscribe({
       next: sessions => actualSessions = sessions
     });
-    const request = controller.expectOne('api/session');
+    const request = controller.expectOne(apiUrl);
     request.flush(expectedSessions);
     expect(actualSessions).toEqual(expectedSessions);
+    controller.verify();
+  });
+
+  it('should get session by id', () => {
+    const expectedSession: Session = { name: 'hello', description: 'world', date: new Date(), teacher_id: 1, users: [1] };
+    var actualSession: Session | undefined;
+    service.detail('id').subscribe({
+      next: session => actualSession = session
+    });
+    const request = controller.expectOne(apiUrl + '/id');
+    request.flush(expectedSession);
+    expect(actualSession).toEqual(expectedSession);
+    controller.verify();
+  });
+
+  it('should delete session', () => {
+    service.delete('id').subscribe({
+      next: _ => {}
+    });
+    const request = controller.expectOne(apiUrl + '/id');
+    controller.verify();
+  });
+
+  it('should create session', () => {
+    const expectedSession: Session = { name: 'hello', description: 'world', date: new Date(), teacher_id: 1, users: [1] };
+    var actualSession: Session | undefined;
+    service.create(expectedSession).subscribe({
+      next: session => actualSession = session
+    });
+    const request = controller.expectOne(apiUrl);
+    request.flush(expectedSession);
+    expect(actualSession).toEqual(expectedSession);
+    controller.verify();
+  });
+
+  it('should update session', () => {
+    const expectedSession: Session = { name: 'hello', description: 'world', date: new Date(), teacher_id: 1, users: [1] };
+    var actualSession: Session | undefined;
+    service.update('id', expectedSession).subscribe({
+      next: session => actualSession = session
+    });
+    const request = controller.expectOne(apiUrl + '/id');
+    request.flush(expectedSession);
+    expect(actualSession).toEqual(expectedSession);
+    controller.verify();
+  });
+
+  it('should participate', () => {
+    service.participate('sessionId', 'userId').subscribe({
+      next: _ => {}
+    });
+    const request = controller.expectOne(apiUrl + '/sessionId/participate/userId');
+    controller.verify();
+  });
+
+  it('should unParticipate', () => {
+    service.unParticipate('sessionId', 'userId').subscribe({
+      next: _ => {}
+    });
+    const request = controller.expectOne(apiUrl + '/sessionId/participate/userId');
+    controller.verify();
   });
 
 });
