1f6a703ab6450092199a714c0fa520e63136bbfb
Geoffrey Ouvrard <geoffreyouvrard@macbook-pro-de-geoffrey.home>
2023-01-31 20:40:00 +0100

    added unit tests for RegisterComponent

--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -6,18 +6,24 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { expect } from '@jest/globals';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
 
 import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let authService: AuthService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RegisterComponent],
       imports: [
+        RouterTestingModule,
         BrowserAnimationsModule,
         HttpClientModule,
         ReactiveFormsModule,  
@@ -25,16 +31,24 @@ describe('RegisterComponent', () => {
         MatFormFieldModule,
         MatIconModule,
         MatInputModule
-      ]
+      ],
+      providers: [AuthService]
     })
       .compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should register', () => {
+    const registerSpy = jest.spyOn(authService, 'register').mockReturnValue(new Observable<void>());
+    component.submit();
+    expect(registerSpy).toHaveBeenCalledTimes(1);
   });
 });
